03d5ab8b8859cee2c0b499f18daf89793405045d
kirtimisal <kirti.misal@perficient.com>
2019-04-29 10:21:02 +0530

    Updated route in manifest.yml with my initials

--- components/movies/src/main/java/org/superbiz/moviefun/movies/ActionServlet.java
+++ components/movies/src/main/java/org/superbiz/moviefun/movies/ActionServlet.java
@@ -14,16 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.superbiz.moviefun;
+package org.superbiz.moviefun.movies;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
-import org.superbiz.moviefun.movies.Movie;
-import org.superbiz.moviefun.movies.MoviesBean;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
